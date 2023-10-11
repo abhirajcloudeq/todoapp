062ce32b7d62f7cee6e23485ae6f8aecc30cd457
Abhiraj Chaudhary <133480538+abhirajcloudeq@users.noreply.github.com>
2023-10-11 13:56:06 +0530

    todo-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <h1 className='h1'>ToDoApp</h1>
+      <div className="add-todo">
+        <input
+          type="text"
+          placeholder="Add Task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button onClick={() =>  setTasks([...tasks, task])}>Add</button>
+      </div>
+      <h3>Tasks</h3>
+      <li className="todo-list">
+        {tasks.map((item, index) => (
+          <li key={index} className="todo-item">
+            <span className="todo-text">{item}</span>
+            
+            <div className="todo-actions">
+              <button
+                className="delete"
+                onClick={() => {
+                  setTasks(tasks.filter((t) => t !== item)); 
+                }}
+              >
+                Delete
+              </button>
+              <div className= "todo-actions">
+              <button onClick={()=>{
+                var newValue = prompt("Edit Todo", tasks[index])
+                setTasks(tasks.map((t)=>{if(t==item) return newValue; else return t;}));
+              }}>Edit</button>
+              </div>
+            </div>
+          </li>
+        ))}
+      </li>
+    </div>           
   );
 }
 
